20fffbe8824d59c687cc3ec43d4df28396cb6e6f
Varga Jenő <jeno_varga@yahoo.com>
2021-04-30 11:12:47 +0200

    JS task ready

--- JS-task/task02-dom/dom.js
+++ JS-task/task02-dom/dom.js
@@ -1,4 +1,23 @@
-function changeOuterLinks() {}
+function changeOuterLinks() {
+  const navElement = document.querySelector('#link-list');
+  const links = navElement.querySelectorAll('#link-list a');
 
-
+  links.forEach((linkElement) => {
+    if (linkElement.innerHTML.includes('outer-link')) {
+      linkElement.setAttribute('target', '_blank');
+      const content = linkElement.innerHTML;
+      linkElement.innerHTML = `<strong>${content}</strong>`;
+    }
+  });
+  navElement.style.display = 'flex';
+  navElement.style.flexDirection = 'column';
+  navElement.style.margin = '0px auto';
+  navElement.style.width = '30%';
+  navElement.style.border = '1px solid blue';
+  navElement.style.padding = '16px';
+}
+  
+changeOuterLinks();
+  
 export { changeOuterLinks };
+  
